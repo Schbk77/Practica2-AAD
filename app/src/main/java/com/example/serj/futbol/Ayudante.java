a1e2ee499566bf5989dd59b516c7d6e3de0a6e47
Sergio Contreras <serconpla@gmail.com>
2014-12-06 21:50:43 +0100

    Initial commit

---
+++ app/src/main/java/com/example/serj/futbol/Ayudante.java
@@ -0,0 +1,89 @@
+package com.example.serj.futbol;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class Ayudante extends SQLiteOpenHelper{
+
+    public static final String DATABASE_NAME = "futbol.sqlite";
+    public static final int DATABASE_VERSION = 1;
+
+    public Ayudante (Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        String query = "CREATE TABLE " + Contrato.TablaJugador.TABLA + " (" +
+                Contrato.TablaJugador._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                Contrato.TablaJugador.NOMBRE + " TEXT UNIQUE, " +
+                Contrato.TablaJugador.TELEFONO + " TEXT, " +
+                Contrato.TablaJugador.FNAC + " TEXT)";
+
+        db.execSQL(query);
+
+        query = "CREATE TABLE " + Contrato.TablaPartido.TABLA + " (" +
+                Contrato.TablaPartido._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                Contrato.TablaPartido.IDJUGADOR + " INTEGER UNIQUE, " +
+                Contrato.TablaPartido.VALORACION + " FLOAT, " +
+                Contrato.TablaPartido.CONTRINCANTE + " TEXT UNIQUE)";
+
+        db.execSQL(query);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // 1 - CREAR TABLAS DE RESPALDO
+        String query = "CREATE TABLE RESPALDO" + Contrato.TablaJugador.TABLA + " (" +
+                Contrato.TablaJugador._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                Contrato.TablaJugador.NOMBRE + " TEXT UNIQUE, " +
+                Contrato.TablaJugador.TELEFONO + " TEXT, " +
+                Contrato.TablaPartido.VALORACION + " FLOAT, " +
+                Contrato.TablaJugador.FNAC + " TEXT)";
+
+        db.execSQL(query);
+
+        // 2 - COPIAR DATOS DE TABLA ORIGINAL A TABLA DE RESPALDO
+        query = "INSERT INTO RESPALDO" + Contrato.TablaJugador.TABLA + " SELECT * FROM " + Contrato.TablaJugador.TABLA;
+        db.execSQL(query);
+
+        // 3 - BORRAR TABLA ORIGINAL
+        query = "DROP TABLE " + Contrato.TablaJugador.TABLA;
+        db.execSQL(query);
+
+        // 4 - CREAR TABLAS NUEVAS
+        onCreate(db);
+
+        // 5 - COPIAR LOS DATOS DE LA TABLA DE RESPALDO A LAS TABLAS NUEVAS
+        query = "INSERT INTO " + Contrato.TablaJugador.TABLA + " (" +
+                Contrato.TablaJugador.NOMBRE + "," +
+                Contrato.TablaJugador.TELEFONO + "," +
+                Contrato.TablaJugador.FNAC + ") SELECT " +
+                Contrato.TablaJugador.NOMBRE + "," +
+                Contrato.TablaJugador.TELEFONO + "," +
+                Contrato.TablaJugador.FNAC + " FROM RESPALDO";
+
+        db.execSQL(query);
+
+        query = "INSERT INTO " + Contrato.TablaPartido.TABLA + " (" +
+                Contrato.TablaPartido.VALORACION + "," +
+                Contrato.TablaPartido.IDJUGADOR + ") SELECT " +
+                Contrato.TablaPartido.VALORACION + "," +
+                Contrato.TablaJugador._ID + " FROM RESPALDO R INNER JOIN " +
+                Contrato.TablaJugador.TABLA + " J WHERE " +
+                "R."+ Contrato.TablaJugador.NOMBRE + "=J." + Contrato.TablaJugador.NOMBRE +
+                " AND R." + Contrato.TablaJugador.TELEFONO + "=J." + Contrato.TablaJugador.TELEFONO +
+                " AND R." + Contrato.TablaJugador.FNAC + "=J." + Contrato.TablaJugador.FNAC;
+
+        db.execSQL(query);
+
+        query = "UPDATE " + Contrato.TablaPartido.TABLA + " SET " + Contrato.TablaPartido.CONTRINCANTE + "=''";
+        db.execSQL(query);
+
+        // 6 - BORRAR TABLAS DE RESPALDO
+        query = "DROP TABLE RESPALDO";
+        db.execSQL(query);
+    }
+}
---
+++ app/src/main/java/com/example/serj/futbol/Jugador.java
@@ -0,0 +1,88 @@
+package com.example.serj.futbol;
+
+import java.io.Serializable;
+
+public class Jugador implements Serializable, Comparable<Jugador>{
+
+    private long id;
+    private String nombre;
+    private String telefono;
+    private String fnac;
+
+    public Jugador(){}
+
+    public Jugador(long id, String nombre, String telefono, String fnac) {
+        this.id = id;
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.fnac = fnac;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getFnac() {
+        return fnac;
+    }
+
+    public void setFnac(String fnac) {
+        this.fnac = fnac;
+    }
+
+    @Override
+    public int compareTo(Jugador another) {
+        if (this.nombre.compareTo(another.nombre)!=0){
+            return this.nombre.compareTo(another.nombre);
+        }else{
+            return this.fnac.compareTo(another.fnac);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Jugador{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", telefono='" + telefono + '\'' +
+                ", fnac='" + fnac + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || ((Object) this).getClass() != o.getClass()) return false;
+
+        Jugador jugador = (Jugador) o;
+
+        if (id != jugador.id) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return (int) (id ^ (id >>> 32));
+    }
+}
